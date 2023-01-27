9d017122cc751d0321abe15772b1270d36778d01
Gabriel Jesus <gabrieljesus9221@gmail.com>
2023-01-27 18:51:49 -0300

    ci: event handler

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 let isModalOpen = false
+let isPlayer1 = true
 
 const modal = document.getElementById("modal")
 
 
 function toggleModal(){
    modal.classList.toggle('modal-open')
+}
+
+function setSpace(event){
+    event.preventDefault()
+    console.log(event.target.innerText = "x")
 }
